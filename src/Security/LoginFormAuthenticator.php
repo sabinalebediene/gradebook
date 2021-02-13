e672b83cc9bcf4c45fe641f3bc1c724f331aee07
sabinalebediene <70894973+sabinalebediene@users.noreply.github.com>
2021-02-13 16:53:31 +0200

    encore, webpack, json, asset, npm

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -96,7 +96,7 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator implements P
             return new RedirectResponse($targetPath);
         }
 
-        return new RedirectResponse($this->urlGenerator->generate('statuses_index'));
+        return new RedirectResponse($this->urlGenerator->generate('student_index'));
         // throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
 
